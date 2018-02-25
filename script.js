b21d7a3aaea48eef80628f2fa142a6bda7406670
Stepha9 <35526741+StephanieNeumann@users.noreply.github.com>
2018-02-25 14:29:11 +0100

    bomb buttons

--- script.js
+++ script.js
@@ -14,9 +14,48 @@ function startTime(){
     var h = today.getHours()
     var m = today.getMinutes()
     var s = today.getSeconds()
+
+
+    m = checkTime(m);
+    s = checkTime(s);
+
     $("#clock").html( h + ":" + m + ":" +s)
+
+    setTimeout(startTime, 500);
 
 }
 
 
 
+function checkTime(number){
+    if(number < 10){
+
+        number = "0" + number;
+
+    }
+
+    return number;
+
+}
+
+
+function startBomb(){
+    var bombSeconds = 10
+    tick(bombSeconds)
+
+}
+
+
+function tick(bombSeconds){
+    bombSeconds = bombSeconds - 1
+    console.log(bombSeconds)
+
+    if(bombSeconds > 0){
+
+        setTimeout(tick(bombSeconds), 1000)
+
+    }else{
+        
+        console.log("EXPLODE!")
+    }    
+} 
